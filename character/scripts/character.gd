43b915850bd954a657f4aa732e6f46077cb2d357
Raniel Lira <Raniel_lima@outlook.com>
2023-08-26 20:39:45 -0300

    feat: Criação da Tela de Menu

--- character/scripts/character.gd
+++ character/scripts/character.gd
@@ -25,5 +25,5 @@ func _move() -> void:
 	velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _friction)
 	velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _friction)
 	
-
-	
+	velocity = _direction.normalized() * _move_speed
+	velocity = _direction.normalized() * _move_speed

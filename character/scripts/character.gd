f71898cba2a30eacf4d474be7769fae7ea963c2c
Andreza <dedezasilva@live.com>
2023-08-29 20:36:30 -0300

    fix: em correção

--- character/scripts/character.gd
+++ character/scripts/character.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var _state_machine
 
 @export_category("Variables")
-@export var _move_speed: float = 69.0
+@export var _move_speed: float = 10.0
 
 @export var _friction: float = 0.2
 @export var _acceleration: float = 0.1
@@ -20,7 +20,7 @@ func _physics_process(_delta: float) -> void:
 	_move()
 	_animate()
 	move_and_slide()
-	
+	## ações para o personagem andar
 func _move() -> void:
 	var _direction: Vector2 = Vector2(
 		Input.get_axis("move_left", "move_right"),
@@ -29,7 +29,7 @@ func _move() -> void:
 	
 	if _direction != Vector2.ZERO:
 		_animation_tree["parameters/idle/blend_position"] = _direction
-	##	_animation_tree["parameters/walk/blend_position", _direction]
+		_animation_tree["parameters/walk/blend_position"] = _direction
 		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)	
 		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)	
 		return 
@@ -41,7 +41,7 @@ func _move() -> void:
 	velocity = _direction.normalized() * _move_speed
 
 func _animate() -> void:
-	if velocity.length() > 20:
+	if velocity.length() > 1:
 		_state_machine.travel("walk")
 		return
 	
